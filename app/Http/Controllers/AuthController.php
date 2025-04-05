ed1e7c2a6c627e0c3fd72266900aef66be063d22
Boluwatifeh <tifeasypeasy@gmail.com>
2025-04-05 14:44:20 +0100

    add validation checks

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,20 +2,28 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     //
     public function register(Request $request)
     {
-        $request->validate([
+        // Validate the request data
+        $validatedData  = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
-
+        if ($validatedData->fails()) {
+            return response()->json($validatedData->errors(), 422);
+        } 
+        // Check if the email already exists
+        $existingUser = User::where('email', $request->email)->first();
+        if ($existingUser) {
+            return response()->json(['message' => 'Email already exists'], 422);
+        }
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
